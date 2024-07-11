f1275f7fdcfbdbc9b93da8a8d56c844199cb725f
Fanji Arief <arieffanji@gmail.com>
2024-07-11 21:18:15 +0700

    refactor: Firebase configuration to be exported separately

--- frontend/src/firebaseConfig.ts
+++ frontend/src/firebaseConfig.ts
@@ -1,32 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken } from "firebase/messaging";
-
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_AUTH_DOMAIN",
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_STORAGE_BUCKET",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  appId: "YOUR_APP_ID"
 };
-
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
-
-export const requestPermission = async () => {
-  Notification.permission
-  try {
-    const permission = await Notification.requestPermission();
-    if (permission === 'granted') {
-      console.log('Notification permission granted.');
-      const serviceWorkerRegistration = await navigator.serviceWorker.register('/firebase-messaging-sw.js');
-      console.log('Service Worker registered:', serviceWorkerRegistration);
-    } else {
-      console.log('Unable to get permission to notify.');
-    }
-  } catch (err) {
-    console.error('An error occurred while retrieving token. ', err);
-  }
-};
-
-export { messaging, getToken };
